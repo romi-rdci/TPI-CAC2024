d4062c2b30d535a0d5966bbe65715a598a20992f
fergeo <fergeo@yahoo.com>
2024-04-25 17:55:48 -0300

    Modificaciones Varias
    Modificaciones en estructura y varias modificaciones mas.

--- js/script.js
+++ js/script.js
@@ -11,16 +11,41 @@ function cargarJSON(url) {
         })
 }
 
-const urlJSON1 = 'anime.json';
-const urlJSON2 = 'series.json';
-const urlJSON3 = 'videoJuegos.json';
+const urlJSON0 = 'peliculasAclamadas.json'
+const urlJSON1 = 'anime.json'
+const urlJSON2 = 'series.json'
+const urlJSON3 = 'videoJuegos.json'
 
 // Cargar los archivos JSON
-Promise.all([cargarJSON(urlJSON1),cargarJSON(urlJSON2),cargarJSON(urlJSON3)])
+Promise.all([cargarJSON(urlJSON0),cargarJSON(urlJSON1),cargarJSON(urlJSON2),cargarJSON(urlJSON3)])
     .then(data => {
-        const data1 = data[0];
-        const data2 = data[1];
-        const data3 = data[2];
+        const data0 = data[0]
+        const data1 = data[1]
+        const data2 = data[2]
+        const data3 = data[3]
+
+        aclamadas = document.querySelector(".peliculasAclamadas")
+
+        data0.aclamadas.forEach( element => {
+
+            divElement = document.createElement('div')
+            imagen = document.createElement('img')
+            imagen.src = element.img
+            imagen.classList.add('imgAclamadas')
+            divElement.appendChild(imagen)
+            aclamadas.appendChild(divElement)
+    
+            divElementTtl = document.createElement('div')
+            h4Element = document.createElement('h4')
+            h4Element.style.fontSize = "25px"
+            h4Element.classList.add('ttlAclamadas')
+            texto = document.createTextNode(element.name)
+            h4Element.appendChild(texto)
+            divElementTtl.appendChild(h4Element)
+            divElement.appendChild(divElementTtl)
+            
+        });
+
 
         anime = document.querySelector(".anime")
 
@@ -45,7 +70,7 @@ Promise.all([cargarJSON(urlJSON1),cargarJSON(urlJSON2),cargarJSON(urlJSON3)])
         });
 
 
-        anime = document.querySelector(".series")
+        series = document.querySelector(".series")
 
         data2.series.forEach( element => {
 
@@ -54,7 +79,7 @@ Promise.all([cargarJSON(urlJSON1),cargarJSON(urlJSON2),cargarJSON(urlJSON3)])
             imagen.src = element.img
             imagen.classList.add('imgAclamadas')
             divElement.appendChild(imagen)
-            anime.appendChild(divElement)
+            series.appendChild(divElement)
     
             divElementTtl = document.createElement('div')
             h4Element = document.createElement('h4')
@@ -68,7 +93,7 @@ Promise.all([cargarJSON(urlJSON1),cargarJSON(urlJSON2),cargarJSON(urlJSON3)])
         });
 
 
-        videoJuegos = document.querySelector(".video-juegos")
+        videoJuegos = document.querySelector(".videoJuegos")
 
         data3.videoJuegos.forEach( element => {
 
