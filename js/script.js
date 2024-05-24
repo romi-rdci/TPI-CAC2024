fcbf6702ec74bf78d3a2eb3ac1d355ce22ecf14f
myburgfranco <myburgfranco@gmail.com>
2024-05-24 20:17:46 -0300

    conexion a api moviedb

--- js/script.js
+++ js/script.js
@@ -117,5 +117,54 @@ Promise.all([cargarJSON(urlJSON0),cargarJSON(urlJSON1),cargarJSON(urlJSON2),carg
 
 
 });
+//consumo de api movie db
+const apiKey = "46463a3dc58678f6131bc5b3d77325aa";
+const url = 'https://api.themoviedb.org/3/movie/now_playing'
+let page = 1
+const url_imagen = "https://image.tmdb.org/t/p/w500";
+const container = document.querySelector(".container .row");
+const next = document.querySelector(".siguiente")
+const prev = document.querySelector(".anterior")
+
+const getData = async ()=>{
+    const response = await fetch(`${url}?api_key=${apiKey}&page=${page}`);
+    const dataJson = await response.json();
+    const results = await dataJson.results;
+    return results;
+}
+
+
+const containerWrite = (results)=>{
+    for (result of results){
+        const movieCard = document.createElement("div");
+        movieCard.innerHTML = `
+                        <div class=""><img class="imgTendencia" src="${url_imagen}${result.poster_path}" alt="Badland Hunters"></div>
+                        <div class=""><a class="ttlMovie" href="./html/pages/detalles.html">${result.title}</a></div>
+        ` ;
+        movieCard.classList.add("cardPelicula")
+        movieCard.classList.add("p-3")
+        container.appendChild(movieCard);
+    }
+} 
+document.addEventListener("DOMContentLoaded",async ()=>{
+    const results = await getData();
+    containerWrite(results.slice(0,8));
+    
+});
+
+next.addEventListener("click",async()=>{
+    
+    const results = await getData();
+    container.innerHTML="";
+    containerWrite(results.slice(8,16));
+    prev.classList.toggle("hide")
+})
+prev.addEventListener("click",async()=>{
+    
+    const results = await getData();
+    container.innerHTML="";
+    containerWrite(results.slice(0,8));
+    prev.classList.toggle("hide")
+})
 
 
